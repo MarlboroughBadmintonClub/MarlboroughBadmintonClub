2015cb900e3e1e396b3bafee67e15e7b79b7f739
Nick Darlow <dangerous.darlow@gmail.com>
2025-06-09 18:30:50 +0100

    Improve membership text clarity. Some readers were interpreting all 6 trial sessions as being free.

--- src/routes/membership/+page.svelte
+++ src/routes/membership/+page.svelte
@@ -5,21 +5,17 @@
 <h1>Membership</h1>
 
 <PaddedBlock>
-  <p>All players are required to be members. Players may attend club nights as a guest member for up to 6 visits before joining.</p>
-</PaddedBlock>
-
-<PaddedBlock>
   <p>
-    The club offers adult and junior memberships. The membership year runs from September to August and there are approximately 40 club nights per year. Annual
-    membership fees are due on 1st September. To qualify for junior membership an individual must be 18 or under on 1st September.
+    There is a fee of £6 for each session you attend. Your first session as a new member is free. After a 6 session trial you must join the club if you wish to
+    continue playing. The club offers adult and junior memberships. To qualify for junior membership an individual must be 18 or under on 1st September 2024.
   </p>
 </PaddedBlock>
 
 <PaddedBlock>
   <p>
     Membership costs £30 per year for adults and is free for juniors. Members registered with other clubs or on the coaching register are entitled to a 50%
-    reduction in the membership fee. There is a fee of £6 for each session attended (club night or match). All members can pay a fixed £140 per year for all
-    club night fees.
+    reduction in the membership fee. All members can pay a fixed £140 per year for all club night fees. Match fees are not included in the £140 per year
+    membership.
   </p>
 </PaddedBlock>
 
@@ -29,4 +25,8 @@
     Banbury Town Badminton League. As a member of Badminton England individuals benefit from £10M public liability insurance cover, a monthly newsletter,
     discounts on badminton purchases from retailers, discounts on Badminton England events and access to Badminton England leagues.
   </p>
+</PaddedBlock>
+
+<PaddedBlock>
+  <p>The membership year runs from September to August and there are approximately 50 club nights per year. Annual membership fees are due on 1st September.</p>
 </PaddedBlock>
